51e8929427468f9811ef1d0326d5ae51d8a2346e
Harshvardhan Dubey <harshvardhan.dubey.it@gmail.com>
2026-06-24 19:57:33 +0530

    feat: create PDF Report Service

---
+++ backend/services/pdf_report_service.py
@@ -0,0 +1,17 @@
+class PDFReportService:
+
+    @staticmethod
+    def generate_executive_pdf(report: dict):
+        ...
+
+    @staticmethod
+    def generate_risk_pdf(report: dict):
+        ...
+
+    @staticmethod
+    def generate_growth_pdf(report: dict):
+        ...
+
+    @staticmethod
+    def generate_board_pdf(report: dict):
+        ...
